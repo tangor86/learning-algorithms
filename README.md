ccdbb6361becf0b8368ea3692c21d1e7b4b086ac
Jevgenijs <tangor@cagoi.com>
2023-03-18 16:36:36 +0200

    bubble sort

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # learning-algorithms
 
-js
+https://quokkajs.com/docs/ ➟
+To open a new Quokka file use Cmd/Ctrl + K, J for JavaScript, or Cmd/Ctrl + K, T for TypeScript.
+To start/restart Quokka on an existing file, use Cmd/Ctrl + K, Q.
---
+++ algo1-bubleSort.js
@@ -0,0 +1,25 @@
+function sortArr(arr) {
+	let tmp
+
+	for (let i = 0; i < arr.length-1; i++) {
+		for (let j = i+1; j < arr.length; j++) {
+		
+			if (arr[j] < arr[i]) {
+				tmp = arr[i]
+				arr[i] = arr[j]
+				arr[j] = tmp
+			}
+
+			arr
+		}
+	}
+}
+
+
+let arr = [222, -23, 1, 10, 0, 123, -1, 0, -100]
+arr
+
+sortArr(arr)
+arr
+
+//console.log('arr = ' + arr)
