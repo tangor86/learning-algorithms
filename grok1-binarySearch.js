fb470d186791c068a25ba56d671c3148802ffa02
Jevgenijs <tangor@cagoi.com>
2023-03-19 12:58:13 +0200

    binary continuation

--- grok1-binarySearch.js
+++ grok1-binarySearch.js
@@ -1,42 +1,51 @@
 //18.03.23
-function binarySearch(arr, x) {
-	let r = null
-	let i = 0
-	let mInd = 1
-	let hInd = arr.length
-	let n = hInd
-	hInd
-
-	let cur = Math.floor(n/2)
-	cur
-	
-	while (cur > 0) {
-		const curVal = arr[cur]
-		curVal
-
-		if ( curVal == x) {
-			return cur-1
-		} else if (curVal > x) {  // <--
-			hInd = cur
-		} else if (curVal < x) {  // -->
-			mInd = cur
-		}
-
-		const d = hInd - mInd
-		d
-		cur = Math.floor(cur/2)
-		cur
-	}
-
-	return r
-}
-
 let arr = [111, -23, 10, 0, 12, 5, -4, 999, -100, 55, 77]
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 arr.sort(function(a, b){return a-b})
 arr
 
-let r = binarySearch(arr, 10)
+let x = 9
+
+function binarySearch(arr, x) {
+	let r = null
+	let i = 0
+	let low = 0
+	let high = arr.length - 1
+
+	arr
+	x
+
+	while (low <= high) {
+		const mid = Math.floor((high + low) / 2)
+		const guess = arr[mid]
+		mid
+		guess
+		low
+		high
+
+		i++
+
+		if (guess == x) {
+			i
+			return mid
+		} else if (guess > x) {  // 1 .. x ... guess ...... 100
+			high = mid - 1
+			high
+		} else if (guess < x) {  // 1 ..... guess ... x ... 100
+			low = mid + 1
+			low
+		}
+	}
+
+	i
+	low
+	high
+	return r
+}
+
+
+
+let r = binarySearch(arr, x)
 r
 
 // for (let i2 = 0; i2 < arr.length; i2++) q{
